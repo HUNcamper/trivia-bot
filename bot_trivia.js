520c0bd94f6616b82c108922dcac07c7c3986783
HUNcamper <selimnahimi@gmail.com>
2016-10-24 20:48:06 +0000

    Half rewritten code

--- bot_trivia.js
+++ bot_trivia.js
@@ -11,8 +11,7 @@ const trigger = '!quiz'; // The message to trigger the quiz
 const answerTrigger = '!a'; // The trigger for the answer message
 const answerTime = 15; // time in seconds to answer a quiz question
 const url = "http://opentdb.com/api.php?amount=1"; // Trivia DB url to fetch the JSON from.
-const correctTime = answerTime * 1000; // Miliseconds		
-				
+const correctTime = answerTime * 1000; // Miliseconds
 
 // Variables
 
@@ -22,93 +21,155 @@ var maxNum = 0; // The biggest number a user can respond with. Global, see above
 var quizChannel; // Current channel the quiz is going in
 var timer; // Timer for the trivia timeout
 
+// Commands
+
+const commands = [
+//	TRIGGER			FUNCTION
+	['!quiz',		'quizTrigger'],
+	['!a',			'answerTrigger'],
+	['!setdiff',	'setDifficulty']
+];
+
+// NOTE: YOU SHOULD NOT REMOVE ANY OF THE ABOVE FUNCTIONS, ONLY MODIFY THEIR TRIGGERS!
+// - IF YOU DO REMOVE ANYTHING, IT WILL BREAK THE BOT! MODIFY AT YOUR OWN RISK!
+// - DO NOT PUT SPACES IN THE TRIGGER NAMES! IT CAN BREAK THE BOT! USE UNDERSCORE ( _ ) INSTEAD!
+
+// Command Help:
+// FUNCTION NAME		ACTION/NOTES
+// -------------	|	------------------
+// quizTrigger 		|	takes no arguments, triggers the quiz/trivia
+// answerTrigger	|	takes 1 argument (1: answer number), triggers the answer
+// setDifficulty	|	takes 1 argument (1: difficulty), set the global difficulty for the quizes/trivias
+
 // Functions
 
 // Escape HTML
-
-var escapeHtml = function(str) {
+// todo: Fix
+function escapeHtml(str) {
 	return str.replace(/&#(\d+);/g, function(match, dec) {
 		return String.fromCharCode(dec);
 	});
 };
 
+// Find an item in a 2D array.
+function findItemArray(needle, hay) {
+    for (var i = 0; i < hay.length; i++) {
+        if (hay[i][0] == needle || hay[i][1] == needle) {
+            return i;
+        }
+    }
+    return false;	// Not found
+}
+
+// Get either a command's trigger or name. See below
+function getCommand(search, mode=0) { // mode 0: return command trigger // mode 1: return function name
+	var _found = commands[findItemArray(search, commands)];
+	
+	if(!_found) return false;
+	
+	switch(mode) {
+		case 0:
+		case 1:
+			return _found[mode];
+			break;
+			
+		default:
+			return false;
+	}
+}
+
+// Check if the user can start a quiz or not
+function checkQuiz(message) {
+	var err = '';
+	if(stillGoing && message.channel != quizChannel)			err='A quiz is already going in ' + quizChannel + ' !';
+	else if(stillGoing)											err='A quiz is already going in this channel!';
+	else return true;
+	
+	if(err != '') {
+		message.reply(err);
+	}
+}
+
+// Check if answer is all correct
+function checkAnswer(message) {
+	var err = '';
+	var msg = message.content.split(" ");
+	var arg1 = parseInt(msg[1]);
+	if(stillGoing && message.channel != quizChannel) 			err='The quiz is going in ' + quizChannel + ' !';
+	else if(!stillGoing) 										err='No quiz is currently going! Start one with !quiz.';
+	else if(typeof msg[1] === 'undefined')						err='No answer was given';
+	else if(isNaN(msg[1]))										err='Answer is not a number';
+	else if(arg1 < 1)											err='Answer is too small';
+	else if(arg1 > maxNum)										err='Answer is too big';
+	else return true;
+	
+	if(err != '') {
+		message.reply(err);
+	}
+}
+
 // Main
 
 bot.on('ready', () => {
 	console.log('Bot Ready');
-	console.log('Trigger command: ' + trigger);
+	console.log('Trigger command: ' + getCommand('quizTrigger'));
 });
 
 bot.on('message', message => {
-	if (stillGoing && message.content.split(" ")[0] == answerTrigger && message.channel != quizChannel) {
-			message.reply('You must reply to the Trivia at ' + quizChannel);
-	} else if (stillGoing && message.content.split(" ")[0] == answerTrigger) {
-		messageSplit = message.content.split(" ");
-		var currChannel = message.channel;
-		// If second argument is defined
-		if(typeof messageSplit[1] !== 'undefined') {
-			// If second argument is integer
-			if(!isNaN(messageSplit[1])) {
-				var num = parseInt(messageSplit[1]);
-				if(num >= 1 && num <= maxNum) {
-					if(num == correctNum) {
-						message.reply('Correct answer! Congratulations!');
-						stillGoing = false;
-						clearTimeout(timer);
-					} else {
-						message.reply('Incorrect answer!');
-					}
-				} else {
-					message.reply('You wrote an incorrect number!');
-				}
-			} else {
-				message.reply('The answer must be a number!');
-			}
-		} else {
-			message.reply('You must write an answer number!');
-		}
-	}
-	if (message.content === trigger) {
-		var currChannel = message.channel;
-		if (stillGoing && message.channel != quizChannel) {
-			message.reply('A Trivia is already going in ' + quizChannel);
-		} else if (stillGoing) {
-			message.reply('Sorry, but a Trivia is already going!'); 
-		} else {
+	// Please note that below is an array
+	var currMessage = message.content.split(" ");
+	
+	if(getCommand(currMessage[0]) !== false) { // Is a command
+		var currCommand = getCommand(currMessage[0], 1); // Get the command name
+		//message.reply('Function for trigger: ' + currCommand);
 		
-		// Fetch the JSON
-		request({
-			url: url,
-			json: true
-		}, function(error, response, body) {
-			if (!error && response.statusCode === 200) {
-				stillGoing = true;
-				quizChannel = currChannel;
-				var triviaMessage = 'Category: *' + escapeHtml(body.results[0].category) + '*, Difficulty: *' + body.results[0].difficulty + '*\n__**' + escapeHtml(body.results[0].question) + '**__';
-				correctNum = Math.floor((Math.random() * body.results[0].incorrect_answers.length) + 1);
-				maxNum = body.results[0].incorrect_answers.length+1;
-				var b = -1;
-				for(var i = 1; i < body.results[0].incorrect_answers.length+2; i++) {
-					if (i == correctNum) {
-						triviaMessage += '\n**[ ' + i + ' ]** ' + escapeHtml(body.results[0].correct_answer);
+		if(currCommand == "quizTrigger") {
+			if(checkQuiz(message)) {
+				request({
+					url: url,
+					json: true
+				}, function(error, response, body) {
+					if (!error && response.statusCode === 200) {
+						stillGoing = true;
+						quizChannel = message.channel;
+						var triviaMessage = 'Category: *' + escapeHtml(body.results[0].category) + '*, Difficulty: *' + body.results[0].difficulty + '*\n__**' + escapeHtml(body.results[0].question) + '**__';
+						correctNum = Math.floor((Math.random() * body.results[0].incorrect_answers.length) + 1);
+						maxNum = body.results[0].incorrect_answers.length+1;
+						var b = -1;
+						for(var i = 1; i < body.results[0].incorrect_answers.length+2; i++) {
+							if (i == correctNum) {
+								triviaMessage += '\n**[ ' + i + ' ]** ' + escapeHtml(body.results[0].correct_answer);
+							} else {
+								b++;
+								triviaMessage += '\n**[ ' + i + ' ]** ' + escapeHtml(body.results[0].incorrect_answers[b]);
+							}
+						}
+						triviaMessage += '\nTime remaining: ' + answerTime + ' seconds\nSend the answer: ' + answerTrigger + '  <number>';
+						
+						message.channel.sendMessage(triviaMessage);
+						
+						timer = setTimeout(function() {
+							message.channel.sendMessage('__**Time is up, the Trivia is over!**__\nThe correct answer was: **' + body.results[0].correct_answer + '**');
+							stillGoing = false;
+						}, correctTime);
 					} else {
-						b++;
-						triviaMessage += '\n**[ ' + i + ' ]** ' + escapeHtml(body.results[0].incorrect_answers[b]);
+						message.reply('Sorry, I failed to connect to the Open Trivia Database :(\nError: ' + response.statusCode);
+						console.log('Failed to fetch from OTD. Response code was: ' + response.statusCode);
 					}
-				}
-				triviaMessage += '\nTime remaining: ' + answerTime + ' seconds\nSend the answer: ' + answerTrigger + '  <number>';
-				
-				currChannel.sendMessage(triviaMessage);
-				
-				timer = setTimeout(function() {
-					currChannel.sendMessage('__**Time is up, the Trivia is over!**__\nThe correct answer was: **' + body.results[0].correct_answer + '**');
-					stillGoing = false;
-				}, correctTime);
-			} else {
-				message.reply('Sorry, I failed to connect to the Open Trivia Database :(');
-				console.log('Failed to fetch from OTD. Response code was: ' + response.statusCode);
+				});
 			}
-		});
+		} else if(currCommand == "answerTrigger") {
+			if(checkAnswer(message)) {
+				// WE ARE ASSUMING that everything returned true. Convert right away.
+				var answer = parseInt(message.content.split(" ")[1]);
+				if(answer == correctNum) {
+					message.reply('Correct answer! Congratulations!');
+					stillGoing = false;
+					clearTimeout(timer);
+				} else {
+					message.reply('Incorrect answer! Try again!');
+				}
+			}
 		}
 	}
 });
